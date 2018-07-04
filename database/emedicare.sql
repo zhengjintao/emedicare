6e14d3478919d85bd62aae8a77361ae2757b234f
zhengjintao <xiaonei0912@qq.com>
2018-07-04 22:49:26 +0900

    テーブル追加

--- database/emedicare.sql
+++ database/emedicare.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `emedicare` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `emedicare`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: localhost    Database: emedicare
@@ -16,6 +14,33 @@ USE `emedicare`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cdata_appointments`
+--
+
+DROP TABLE IF EXISTS `cdata_appointments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cdata_appointments` (
+  `no` int(11) NOT NULL,
+  `userid` varchar(30) DEFAULT NULL,
+  `appointdate` datetime DEFAULT NULL,
+  `header` varchar(50) DEFAULT NULL,
+  `content` longtext,
+  `status` char(1) DEFAULT NULL,
+  PRIMARY KEY (`no`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cdata_appointments`
+--
+
+LOCK TABLES `cdata_appointments` WRITE;
+/*!40000 ALTER TABLE `cdata_appointments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cdata_appointments` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `cdata_userlog`
@@ -39,6 +64,31 @@ CREATE TABLE `cdata_userlog` (
 LOCK TABLES `cdata_userlog` WRITE;
 /*!40000 ALTER TABLE `cdata_userlog` DISABLE KEYS */;
 /*!40000 ALTER TABLE `cdata_userlog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cdata_visithistory`
+--
+
+DROP TABLE IF EXISTS `cdata_visithistory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cdata_visithistory` (
+  `no` int(11) NOT NULL,
+  `userid` varchar(30) DEFAULT NULL,
+  `visitdate` date DEFAULT NULL,
+  `content` longtext,
+  PRIMARY KEY (`no`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='通院履歴テーブル';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cdata_visithistory`
+--
+
+LOCK TABLES `cdata_visithistory` WRITE;
+/*!40000 ALTER TABLE `cdata_visithistory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cdata_visithistory` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -84,4 +134,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-30 21:51:36
+-- Dump completed on 2018-07-04 22:47:24
