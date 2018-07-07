2437dea6445e042ba1869a0ef69dded9a1c70a60
zhengjintao <xiaonei0912@qq.com>
2018-07-07 19:16:31 +0900

    ログアウト処理

---
+++ src/main/java/com/bwc/biz/emedicare/servlets/LogoutServlet.java
@@ -0,0 +1,58 @@
+package com.bwc.biz.emedicare.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 删除session
+		HttpSession session = request.getSession();
+		session.removeAttribute("userId");
+		session.invalidate();
+		
+		// 删除Cookies
+		Cookie[] cookies = request.getCookies();
+
+		if (cookies != null) {
+			for (int i = 0; i < cookies.length; i++) {
+				Cookie ck = cookies[i];
+				
+				if ("ucookies".equals(ck.getName()) || "pcookies".equals(ck.getName())) {
+					ck.setValue("");
+					ck.setMaxAge(-1);
+					response.addCookie(ck);
+				}
+			}
+		}
+		
+		response.sendRedirect("login.do");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- src/main/webapp/bkheader.jsp
+++ src/main/webapp/bkheader.jsp
@@ -15,6 +15,35 @@
 		<a class="active item" href=bkaccountlist.jsp><b>顧客一覧</b></a> <a
 			class="item" href=bkimportinfo.jsp><b>診断情報一括登録</b></a> <a
 			class="item" href=bknextplan.jsp><b>次回検査</b></a>
+			
+			<div class="right menu">
+			<a class="item"  onclick="logout()"><b>ログアウト</b></a>
+			
+			<div id="almodal" class="ui small test modal transition hidden">
+					<div class="header">ログアウト</div>
+					<div class="content">
+						<p>ログアウトしてよろしいですか</p>
+					</div>
+					<div class="actions">
+						<div class="ui negative button">いいえ</div>
+						<div class="ui positive right labeled icon button">
+							はい <i class="checkmark icon"></i>
+						</div>
+					</div>
+				</div>
+				<script type="text/javascript">
+					function logout() {
+						$('#almodal').modal({
+							closable : false,
+							onApprove : function() {
+								window.location.href = "logout.do";
+							}
+
+						}).modal('show');
+					}
+				</script>
+				
+		</div>
 	</div>
 </body>
 </html>
